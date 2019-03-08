516abd7d64d2bb5a29a7a6e4b9f1d1f6764733df
Tobias Koltsch <tobias@koltsch.de>
2019-03-08 13:15:48 +0100

    Initial Spring Boot application

---
+++ src/main/java/org/nerdcoding/firstuploader/auth/FirstUploaderAuthApplication.java
@@ -0,0 +1,13 @@
+package org.nerdcoding.firstuploader.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstUploaderAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstUploaderAuthApplication.class, args);
+    }
+
+}
